dda2c88e05ce8ea397d51105f34bde865e45ac55
jcastil1o <157521655+jcastil1o@users.noreply.github.com>
2024-11-06 12:55:38 -0600

    Update BD.sql

--- GUADALUPANA/BD.sql
+++ GUADALUPANA/BD.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS `GUADALUPANA`.`datos`
     `Apellido` VARCHAR(20),
     `Direccion` VARCHAR(100),
     `Telefono` VARCHAR(20),
+    `Plaza` VARCHAR(35),
     PRIMARY KEY (id_datos),
     CONSTRAINT CUI FOREIGN KEY (CUI)
     REFERENCES GUADALUPANA.Escolaridad(CUI)
@@ -19,3 +20,17 @@ CREATE TABLE IF NOT EXISTS `GUADALUPANA`.`Escolaridad`
     `Grado` TEXT NOT NULL,
     PRIMARY KEY (CUI)
 );
+CREATE TABLE IF NOT EXISTS `GUADALUPANA`.`pruebas`
+(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            Mensaje LONGTEXT,
+            Tiempo TIMESTAMP
+);
+SELECT * FROM  `GUADALUPANA`.`datos`;
+SELECT * FROM  `GUADALUPANA`.`Escolaridad`;
+SELECT * FROM `GUADALUPANA`.`pruebas`;
+SELECT 
+	datos.id_datos, datos.CUI, datos.Nombre, datos.Plaza
+FROM `GUADALUPANA`.`datos` 
+INNER JOIN `GUADALUPANA`.`Escolaridad` 
+ON datos.CUI = Escolaridad.CUI;
